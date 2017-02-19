3a87dc7b03d3c8b31042739a0c0f9c3c801d734a
L1n <490772448@qq.com>
2017-02-19 13:25:31 +0800

    更新一下 2017.02.19 弟弟要做的练习题, 先更新两道题

--- 题目16/solve16.py
+++ 题目16/solve16.py
@@ -5,8 +5,8 @@
 2017.02.19 题 16
     1. 删除空格
     2. 回文数判定
-    3.
-    4.
+    3. 二维转一维
+    4. 数字字符前移
 """
 __author__ = '__L1n__w@tch'
 
@@ -21,7 +21,7 @@ class Solve:
         :param a_str: str(), 比如 "asd af aa z67"
         :return: str(), 删除空格后的字符串, 比如 "asdafaaz67"
         """
-        return a_str.replace(" ", "")
+        pass
 
     @staticmethod
     def is_hui_wen(a_str):
@@ -36,7 +36,29 @@ class Solve:
         :param a_str: str(), 待判断的字符串, 比如 "LEVEL"
         :return: boolean(), True or False, 是回文则返回 True, 否则返回 False
         """
-        return a_str == a_str[::-1]
+        pass
+
+    @staticmethod
+    def matrix_to_list(matrix):
+        """
+        将 M 行 N 列的二维数组中的数据, 按行的顺序依次放到一维数组中
+        :param matrix: list(), 矩阵, 比如 [[33, 33, 33, 33], [44, 44, 44, 44], [55, 55, 55, 55]]
+        :return: list(), 列表, 比如 [33, 33, 33, 33, 44, 44, 44, 44, 55, 55, 55, 55]
+        """
+        pass
+
+    @staticmethod
+    def digits_pre_move(a_str):
+        """
+        将字符串中的所有数字字符顺序前移, 其他字符顺序后移
+        参考思路:
+            (1)用列表分别存放数字和其他字符
+            (2)遍历字符串中每个字符, 如果是数字则放在数字列表中, 否则存在另一个列表中, 参考 str().isdigit() 函数
+            (3)将两个列表拼接起来后返回, 参考: "".join(list)
+        :param a_str: str(), 比如 "asd123fgh543df"
+        :return: str(), 比如 "123543asdfghdf"
+        """
+        pass
 
 
 if __name__ == "__main__":
--- 题目16/test_question16.py
+++ 题目16/test_question16.py
@@ -5,8 +5,8 @@
 2017.02.19 题 16
     1. 删除空格
     2. 回文数判定
-    3.
-    4.
+    3. 二维转一维
+    4. 数字字符前移
 """
 import unittest
 import random
@@ -54,6 +54,63 @@ class TestSolve(unittest.TestCase):
                 test_str += test_str[::-1]
             my_answer = self.test_solve.is_hui_wen(test_str)
             self.assertEqual(right_answer, my_answer)
+
+    def test_matrix_to_list(self):
+        test_data = [[33, 33, 33, 33], [44, 44, 44, 44], [55, 55, 55, 55]]
+        right_answer = [33, 33, 33, 33, 44, 44, 44, 44, 55, 55, 55, 55]
+        my_answer = self.test_solve.matrix_to_list(test_data)
+        self.assertEqual(right_answer, my_answer)
+
+        for i in range(10 ** 2):
+            test_matrix = self.create_random_matrix()
+            right_answer = self.matrix_to_list(test_matrix)
+            my_answer = self.test_solve.matrix_to_list(test_matrix)
+            self.assertEqual(right_answer, my_answer)
+
+    def test_digits_pre_move(self):
+        test_data = "asd123fgh543df"
+        right_answer = "123543asdfghdf"
+        my_answer = self.test_solve.digits_pre_move(test_data)
+        self.assertEqual(right_answer, my_answer)
+
+        for i in range(10 ** 2):
+            test_data = "".join([random.choice(string.ascii_letters + string.digits) for x in range(1, 10 ** 4)])
+            right_answer = self.digits_pre_move(test_data)
+            my_answer = self.test_solve.digits_pre_move(test_data)
+            self.assertEqual(right_answer, my_answer)
+
+    @staticmethod
+    def digits_pre_move(a_str):
+        num_list = list()
+        other_list = list()
+        for each_char in a_str:
+            if each_char.isdigit():
+                num_list.append(each_char)
+            else:
+                other_list.append(each_char)
+
+        return "".join(num_list + other_list)
+
+    @staticmethod
+    def matrix_to_list(matrix):
+        result = list()
+        for each in matrix:
+            result += each
+        return result
+
+    @staticmethod
+    def create_random_matrix():
+        n = random.randint(1, 10)
+        m = random.randint(1, 10)
+
+        result_list = list()
+        for i in range(n):
+            temp_list = list()
+            for j in range(m):
+                temp_list.append(random.randint(1, 10 ** 3))
+            result_list.append(temp_list)
+
+        return result_list
 
 
 if __name__ == "__main__":
